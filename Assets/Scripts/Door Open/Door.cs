1fa0f5da2edc61614dbdf975c1cda4dabc2c481f
Vladis Kosta <Vladislarnaka@gmail.com>
2022-07-05 20:04:11 +0300

    Added a new door system

---
+++ Assets/Scripts/Door Open/Door.cs
@@ -0,0 +1,80 @@
+// Created by Vladis.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///     What does this Door do?
+/// </summary>
+public sealed class Door : MonoBehaviour
+{
+    // the layer of the checkbox
+    [SerializeField]
+    private LayerMask layer = 9;
+    // DistanceBetweenPlayerAndDoor
+    private Vector3 OffsetForCheckBox;
+    // variables
+    private Vector3 EndPos;
+    private Vector3 StartPos;
+    private bool CanActiveOrNot = true;
+    private void Start()
+    {
+        StartPos = transform.position;
+        EndPos = new Vector3(transform.position.x, transform.position.y + 3, transform.position.z);
+        OffsetForCheckBox = new Vector3(transform.position.x, transform.position.y - 3, transform.position.z);
+
+    }
+    public void OpenDoor()
+    {
+        if(CanActiveOrNot == true)
+        {
+         StartCoroutine(IenumOpenDoor());
+            CanActiveOrNot = false;
+        }
+        
+    }
+    private IEnumerator IenumOpenDoor()
+    {
+        float TimeUntllDoor = 0.0f;
+        float FixTimer = 0.0f;
+
+        while (transform.position != EndPos)
+        {
+            TimeUntllDoor += 0.3f * Time.deltaTime;
+            transform.position = Vector3.Lerp(transform.position, EndPos, TimeUntllDoor / 1.0f);
+            yield return new WaitForFixedUpdate();
+        }
+        TimeUntllDoor = 0.0f;
+
+        while (transform.position != StartPos)
+        {
+
+            bool checkbox = Physics.CheckBox(OffsetForCheckBox, transform.localScale, transform.rotation, layer);
+            if (checkbox)
+            {
+
+                TimeUntllDoor = 0;
+                FixTimer += 0.3f * Time.deltaTime;
+                transform.position = Vector3.Lerp(transform.position, EndPos, FixTimer / 1.0f);
+            }
+            else
+            {
+                FixTimer = 0;
+                TimeUntllDoor += 0.3f * Time.deltaTime;
+                transform.position = Vector3.Lerp(transform.position, StartPos, TimeUntllDoor / 1.0f);
+            }
+            //  yield return new WaitUntil(() => (Vector3.Distance(CurrentPosPlayer , currentpos) < 0.7f));
+
+            yield return new WaitForFixedUpdate();
+        }
+        CanActiveOrNot = true;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(StartPos, EndPos);
+
+        //Debug.DrawLine(transform.position, transform.position + transform.up * -2);
+      //  Gizmos.DrawCube(transform.position + transform.up * -2, transform.localScale);
+    }
+}
---
+++ Assets/Scripts/Door Open/DoorManager.cs
@@ -0,0 +1,40 @@
+// Created by Vladis.
+
+//using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///     What does this DoorManager do?
+/// </summary>
+public sealed class DoorManager : MonoBehaviour
+{
+    private List<Door> Doors;
+    [SerializeField]
+    private Transform Player;
+    public Transform test; 
+    private void Start()
+	{
+        Doors = new List<Door>();
+		for(int i = 0 ; i < transform.childCount; i++)
+        {
+            Doors.Add(transform.GetChild(i).GetComponent<Door>());
+        }
+	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            foreach(Door door in Doors)
+            {
+                if((Player.position - door.transform.position).magnitude < 3)
+                {
+
+                     door.OpenDoor();
+                    
+                }
+            }
+        }
+    }
+}
