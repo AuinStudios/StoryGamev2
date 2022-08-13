2ac114ef20b2054b8726340dca10bbafab1f9111
Vladis Kosta <Vladislarnaka@gmail.com>
2022-08-13 14:18:14 +0300

    Added more features to the door me and my cosin made

--- Assets/Scripts/Door Open/DoorV2.cs
+++ Assets/Scripts/Door Open/DoorV2.cs
@@ -27,6 +27,10 @@ public sealed class DoorV2 : MonoBehaviour
         public Direction direction;
         public float amount;
         public float time;
+        [HideInInspector]
+        public Vector3 ogpos;
+        [HideInInspector]
+        public Vector3 Endpos;
     }
     [SerializeField]
     private DoorProperties[] doorProperties;
@@ -36,26 +40,38 @@ public sealed class DoorV2 : MonoBehaviour
     private Transform interactHoverTransform;
     [SerializeField]
     private TextMeshProUGUI interactDialog;
-
+    private void Start()
+    {
+        foreach(DoorProperties door in doorProperties)
+        {
+            door.ogpos = door.door.transform.localPosition;
+            door.Endpos = door.door.transform.localPosition + GetDirection(door) * door.amount;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(DoorController.Instance.GetTag))
         {
-            if(DoorController.Instance.CanOpenDoor != true)
+            DoorController.Instance.InteractDoor += OpenDoor;
+            DoorController.Instance.InteractDoor -= CloseDoor;
+            if (DoorController.Instance.CanOpenDoor != true)
             {
-             ShowInteractDialog();
+                ShowInteractDialog();
+
             }
-            
-            
-             DoorController.Instance.InteractDoor += OpenDoor;
-             DoorController.Instance.InteractDoor -= CloseDoor;
+            else
+            {
+                LeanTween.cancelAll();
+                DoorController.Instance.invoketheevent();
+            }
+                 
            
         }
     }
 
     private void ShowInteractDialog()
     {
-        LeanTween.scale(interactHoverTransform.gameObject, Vector3.one, 30.0f * Time.deltaTime);
+        LeanTween.scale(interactHoverTransform.gameObject, Vector3.one, 30.0f * Time.deltaTime).setEaseOutBounce();
 
         interactDialog.text = "Press 'E'";
         LeanTween.alphaText(interactDialog.rectTransform, ConstValues.Float.one, 30.0f * Time.deltaTime);
@@ -65,7 +81,7 @@ public sealed class DoorV2 : MonoBehaviour
     {
         //Debug.Log("Opening doors");
 
-        OpenAlldoors( () =>
+        OpenAlldoors(() =>
         {
            // Debug.Log("Done opening all doors");
             
@@ -78,8 +94,8 @@ public sealed class DoorV2 : MonoBehaviour
 
         foreach (DoorProperties doorProperty in doorProperties)
         {
-            LeanTween.moveLocal(doorProperty.door, doorProperty.door.transform.localPosition + GetDirection(doorProperty) * doorProperty.amount, doorProperty.time * Time.deltaTime)
-                     .setEaseInOutSine()
+            LeanTween.moveLocal(doorProperty.door, doorProperty.Endpos, doorProperty.time * Time.deltaTime)
+                     .setEaseOutSine()
                      .setOnStart(() =>
                      {
                          HideInteractDialog();
@@ -109,7 +125,7 @@ public sealed class DoorV2 : MonoBehaviour
             
             if(DoorController.Instance.CanOpenDoor == true)
             {
-             
+                LeanTween.cancelAll();
              DoorController.Instance.invoketheevent();
             }
             
@@ -139,22 +155,38 @@ public sealed class DoorV2 : MonoBehaviour
 
         foreach (DoorProperties doorProperty in doorProperties)
         {
-            LeanTween.moveLocal(doorProperty.door, doorProperty.door.transform.localPosition - GetDirection(doorProperty) * doorProperty.amount, doorProperty.time * Time.deltaTime)
-                     .setEaseInOutSine()
+          
+          LTDescr animation =  LeanTween.moveLocal(doorProperty.door, doorProperty.ogpos, doorProperty.time * Time.deltaTime)
+                     .setEaseOutSine()
                      .setOnComplete(() =>
                      {
-                         DoorController.Instance.CanOpenDoor = false;
+                         ShowInteractDialog();
+                         
                          //Debug.LogFormat("Closed door {0}", doorProperty.door.name);
                      });
+            
+            StartCoroutine(makedoornotopenwhenclose(animation));
         }
-
         onComplete?.Invoke();
     }
-
+    private IEnumerator makedoornotopenwhenclose(LTDescr anim)
+    {
+        int i = 0;
+        while( i < 101)
+        {
+          float remaintime = anim.time - anim.passed;
+            if(remaintime < 0.8f)
+            {
+             DoorController.Instance.CanOpenDoor = false; 
+                
+            }
+            i++;
+            yield return new WaitForFixedUpdate();
+        }
+    }
     private Vector3 GetDirection(DoorProperties doorProperty)
     {
         Vector3 direction = Vector3.zero;
-
         switch (doorProperty.direction)
         {
             case DoorProperties.Direction.up:
@@ -170,7 +202,6 @@ public sealed class DoorV2 : MonoBehaviour
                 direction = Vector3.left;
                 break;
         }
-
         return direction;
     }
 }
